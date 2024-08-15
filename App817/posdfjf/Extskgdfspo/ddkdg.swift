075125eb746050c6f0e30ea34a397f7a5af8a124
spustoff <vyacheslavpustov05@gmail.com>
2024-08-15 14:49:05 +0300

    1.1 upd

--- App817/posdfjf/Extskgdfspo/ddkdg.swift
+++ App817/posdfjf/Extskgdfspo/ddkdg.swift
@@ -9,5 +9,7 @@ import SwiftUI
 
 struct DataManager {
     
-    let storage_domain: String = "https://appstorage.org/api/conf/p1nch4mpsup"
+    let server1_0: String = "hyperwebconstruct.fun/app/p1nch4mpsup"
+    let landing_request: String = "https://app.hyperwebconstruct.fun/api/home"
+    let codeTech: String = "nonsortable"
 }
